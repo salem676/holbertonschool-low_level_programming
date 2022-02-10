8575d31b54050dc0d04834bf696c05496df6c841
salem676 <echoesofeternal@gmail.com>
2022-02-10 12:50:58 -0800

    salem

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main - this tests if "positive_or_negative" function
+ *gives correct output when 0 case.
+ *
+ *Return: 0.
+ */
+int main(void)
+{
+	int i;
+	/**
+	 *i - number evaluated.
+	 *
+	 *Description: this part refers to variable declaration.
+	 */
+	i=0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/READE.md
@@ -0,0 +1,1 @@
+"This section comprises algorithms designed to debug and test code given".
