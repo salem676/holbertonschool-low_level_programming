950c9a0c75125518a013c73210531deb29920d91
salem676 <echoesofeternal@gmail.com>
2022-02-11 12:54:10 -0800

    using betty to check

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check if variabe is uppercase.
- *
+ * _isupper - check if variabe is uppercase.
+ * @c: is the character to check.
+ * Description: Function that checks if character is uppercase.
  * Return: 1 if true and 0 if false.
- *
  */
 int _isupper(int c)
 {
-	/**
-	 * c - is the character to check.
-	 *
-	 * Description: this part refers to variable declaration.
-	 */
 	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
