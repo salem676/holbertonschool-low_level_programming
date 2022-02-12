500c7faec6e79ded3e7b2f61594d35215755d892
salem676 <echoesofeternal@gmail.com>
2022-02-12 08:04:23 -0800

    drink

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
+#include "main.h"
 /**
- * print_numbers - prints numbers from 0 to 9.
- *
+ * print_most_numbers - prints numbers from 0 to 9.
+ * Description: this function prints numbers from 0 to 9.
+ * Also prints space where needed (instructions).
  * Return: void.
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 	/**
-	 * i - counter and number to be printed.
+	 * @i: counter and number to be printed.
 	 *
 	 * Description: this part refers to variable description.
 	 */
-	for (i = 1; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		if ((i != 2) && (i != 4))
 		{
-			putchar(i);
-			putchar('\n');
+			_putchar(i + 48);
 		}
 	}
+	_putchar('\n');
 }
