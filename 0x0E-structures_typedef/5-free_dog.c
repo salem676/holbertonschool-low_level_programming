7bc4fc97b9d76a7fc4c0ed624eea1f44f0de2557
salem676 <echoesofeternal@gmail.com>
2022-03-08 12:48:46 -0800

    next

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+/*
+ * File: 5-free_dog.c
+ * Author: salem676
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - function that frees dogs.
+ * @d: pointer argument passed from main.
+ * Description: function that frees dogs.
+ * Return: void.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
